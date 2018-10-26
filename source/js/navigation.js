41685da2331a17b3b54bc7ad27615c12a4da8793
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2018-10-27 00:02:50 +0300

    Перевел меню в исходное состояние и добавил скрипт открытия и закрытия

---
+++ source/js/navigation.js
@@ -0,0 +1,14 @@
+var nav = document.querySelector(".main-nav");
+var toggle = document.querySelector(".main-nav__toggle");
+
+nav.classList.remove("main-nav--nojs");
+
+toggle.addEventListener("click", function () {
+  if (nav.classList.contains("main-nav--closed")) {
+    nav.classList.remove("main-nav--closed");
+    nav.classList.add("main-nav--opened");
+  } else {
+      nav.classList.remove("main-nav--opened");
+      nav.classList.add("main-nav--closed");
+  }
+});
