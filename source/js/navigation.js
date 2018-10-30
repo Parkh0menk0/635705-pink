d72381e59c32e9898f403560eedafa64998d6e15
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2018-10-30 22:10:25 +0200

    Поправил js код открытия (закрытия) меню проекта Пинк

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -3,12 +3,17 @@ var toggle = document.querySelector(".main-nav__toggle");
 
 nav.classList.remove("main-nav--nojs");
 
-toggle.addEventListener("click", function () {
-  if (nav.classList.contains("main-nav--closed")) {
+toggle.addEventListener("click", function (evt) {
+  evt.preventDefault;
+  if(nav.classList.contains("main-nav--closed")) {
     nav.classList.remove("main-nav--closed");
     nav.classList.add("main-nav--opened");
+    this.classList.remove("main-nav__toggle--opened");
+    this.classList.add("main-nav__toggle--closed");
   } else {
-      nav.classList.remove("main-nav--opened");
-      nav.classList.add("main-nav--closed");
+    nav.classList.remove("main-nav--opened");
+    nav.classList.add("main-nav--closed");
+    this.classList.remove("main-nav__toggle--closed");
+    this.classList.add("main-nav__toggle--opened");
   }
 });
